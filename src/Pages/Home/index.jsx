8cba1b8c72cbcbb2c0ccb045df9a9590f5eb9fd2
gustavo.rañieski <gustavoranieski@hotmail.com>
2023-11-19 11:30:15 -0300

    mejorando el llamado a  API Fake Store

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,30 +1,37 @@
 import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
+import { apiUrl } from "../../config/apiConfig";
 
 function Home() {
   const [items, setItems] = useState(null)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products/')
+    fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-      console.log(data); 
       setItems(data)
+      setLoading(false)
     })
-    
+    .catch(error => {
+      console.error('Error fetching data:', error)
+      setLoading(false)
+    }); 
   }, [])
 
   return (
     <Layout>
       Home
-      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
-        {
-          items?.map(item => (
-            <Card key={item.id} data={item} />
-          ))
-        } 
-      </div>
+      {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+            {items?.map(item => (
+              <Card key={item.id} data={item} />
+            )) || "No items to display"}
+          </div>
+        )}
     </Layout>
   );
 }
