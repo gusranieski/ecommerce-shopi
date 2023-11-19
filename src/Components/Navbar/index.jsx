ea0efa0d9fd0dd2a15830622013bfdbc0b9d8fa1
gustavo.rañieski <gustavoranieski@hotmail.com>
2023-11-19 12:29:25 -0300

    contador de carrito

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { ShoppingCartContext } from "../../Context";
 
 const Navbar = () => {
+  const context = useContext(ShoppingCartContext)
+
   const activeStyle = "underline underline-offset-4";
 
   return (
@@ -86,7 +90,7 @@ const Navbar = () => {
             Sign In
           </NavLink>
         </li>
-        <li>🛒 0</li>
+        <li>🛒 {context.count}</li>
       </ul>
     </nav>
   );
