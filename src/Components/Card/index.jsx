bc443e95250aa5411df283a264200bda201ef84a
gustavo.rañieski <gustavoranieski@hotmail.com>
2023-11-21 10:58:49 -0300

    agregando estilos a detail

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -28,7 +28,7 @@ const Card = (data) => {
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category}</span>
                 <img className='w-full h-full object-cover rounded-lg' src={data.data.image} alt={data.data.title} />
                 <div 
-                    className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-2'
+                    className='absolute top-0 right-0 flex justify-center items-center bg-sky-100 hover:bg-sky-300 w-6 h-6 rounded-full m-2 p-1'
                     onClick={(event) => addProductsToCart(event, data.data)}>
                     <PlusIcon className='h-6 w-6 text-black' />
                 </div>
