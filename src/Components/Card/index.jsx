c109a4b373ff737826b1673cbe7fc04a77f9ae32
gustavo.rañieski <gustavoranieski@hotmail.com>
2024-03-30 10:01:12 -0300

    agregando componente ordercard

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -16,8 +16,6 @@ const Card = (data) => {
         context.setCartProducts([...context.cartProducts, productData])
         context.openCheckoutSideMenu()
         context.closeProductDetail()
-
-        console.log('CART:', context.cartProducts)
     }
 
     return (
@@ -26,7 +24,7 @@ const Card = (data) => {
             onClick={() => showProduct(data.data)}>
             <figure className='relative mb-2 w-full h-4/5'>
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category}</span>
-                <img className='w-full h-full object-cover rounded-lg' src={data.data.image} alt={data.data.title} />
+                <img className='w-full h-full object-contain rounded-lg' src={data.data.image} alt={data.data.title} />
                 <div 
                     className='absolute top-0 right-0 flex justify-center items-center bg-sky-100 hover:bg-sky-300 w-6 h-6 rounded-full m-2 p-1'
                     onClick={(event) => addProductsToCart(event, data.data)}>
